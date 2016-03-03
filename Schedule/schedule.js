c4554bb8c74f4c653eebb0489fbade000687bbe8
brandonKendall <smoothbmk@gmail.com>
2016-03-02 23:41:13 -0500

    somehow formatted dont even know how

--- Schedule/schedule.js
+++ Schedule/schedule.js
@@ -387,7 +387,7 @@ var rows = [
         "topic": "Final project Presentation",
         "project": " ",
         "notes": " "
-    }, 
+    },
 
     {
         "class": " ",
@@ -415,21 +415,4 @@ $(document).ready(function() {
     });
 });}
 
-// Test
-//var jsonData = '[{"rank":"9","content":"Alon","UID":"5"},{"rank":"6","content":"Tala","UID":"6"}]';
-
-//$.ajax({
-//    url: '/echo/json/',
-//    type: 'POST',
-//    data: {
-//        json: jsonData
-//    },
-//    success: function (response) {
-//        var trHTML = '';
-//        $.each(response, function (i, item) {
-//            trHTML += '<tr><td>' + item.rank + '</td><td>' + item.content + '</td><td>' + item.UID + '</td></tr>';
-//        });
-//        $('#records_table').append(trHTML);
-//    }
-//});
-//};
+$('#schedule').css("border", "double");
