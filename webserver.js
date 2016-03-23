8b4ea43a6f633f23cd106daf83c9e71bc47c8476
TylerMosakowski <tjm5702@psu.edu>
2016-03-22 22:41:16 -0400

    Added files via upload

--- webserver.js
+++ webserver.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var bodyParser  = require('body-parser');
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+var port = process.argv[2] || 8080;
 
 var ChatServer  = require('./CloudChat/ChatServer');
 var syllabus  = require('./Syllabus/syllabus');
@@ -105,3 +110,31 @@ app.get('/Syllabus/*', syllabus.gettool);
 app.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:8080/');
 });
+
+function GET_Request_Handler(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, { "Content-Type": "text/plain" });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, { "Content-Type": "text/plain" });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}
